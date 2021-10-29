36deb880a5ac17ee7e6f6574149a56bc87aaeac8
Abdallah <abdallah.m@padoa-group.com>
2021-10-29 17:58:40 +0200

    updater

--- update-check.js
+++ update-check.js
@@ -1,6 +1,13 @@
 const simpleGit = require('simple-git');
 const git = simpleGit();
 
-git.diff((diff) => {
-  console.log('dif: ', diff);
-})
+const test = async () => {
+  const wholeRepoStatus = await git.status();
+  console.log("wholeRepoStatus", wholeRepoStatus)
+  if (wholeRepoStatus.ahead || wholeRepoStatus.behind) {
+    await git.stash()
+    await git.pull('origin', 'master', {'--rebase': 'true'})
+  }
+};
+
+test();
