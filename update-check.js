4e82d1a33db213c3e8106109323d5b7eadf62f57
Abdallah <abdallah.m@padoa-group.com>
2021-10-29 18:01:38 +0200

    fix updater

--- update-check.js
+++ update-check.js
@@ -2,12 +2,17 @@ const simpleGit = require('simple-git');
 const git = simpleGit();
 
 const test = async () => {
+  try {
+    await git.init().addRemote('origin', '...remote.git');
+  } catch(e){};
+
   const wholeRepoStatus = await git.status();
-  console.log("wholeRepoStatus", wholeRepoStatus)
   if (wholeRepoStatus.ahead || wholeRepoStatus.behind) {
-    await git.stash()
-    await git.pull('origin', 'master', {'--rebase': 'true'})
+    console.log('Uploading new version');
+    await git.stash();
+    await git.pull('origin', 'master', {'--rebase': 'true'});
   }
+  console.log('Version up to date');
 };
 
 test();
