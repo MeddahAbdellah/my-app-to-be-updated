dbd7abe4f2e073c436a275c96a32510b49a770ae
Abdallah <abdallah.m@padoa-group.com>
2021-11-04 13:39:07 +0100

    fix

--- index.js
+++ index.js
@@ -4,34 +4,37 @@ const { exec } = require('pkg');
 const launchApp = require('child_process').spawn;
 var pjson = require('./package.json');
 
-console.log("App version: ", pjson.version);
 
-const checkUpdate = async () => {
-  try {
-    if(!process.argv) return;
-    if(!process.argv.slice(-1)[0]) return;
-    await git.init().addRemote('origin', process.argv.slice(-1)[0]);
-  } catch(e){
-    console.log('Couldnt set origin: ', e);
-  };
-  await git.fetch();
-  const wholeRepoStatus = await git.status();
-  if (wholeRepoStatus.ahead || wholeRepoStatus.behind) {
-    console.log('Uploading new version');
-    await git.stash();
-    await git.pull('origin', 'master', {'--rebase': 'true'});
-    await exec(['index.js', '--target', 'host', '--output', 'app']);
-    await launchApp('./app', (err, stdout, stderr) => {
-        if(err) {
-          console.log(err);
-          return;
-        }
-        console.log('success');
-      });
-    return;
-  }
-  setInterval(() => {}, 10000);
-};
-
-checkUpdate();
+(function main() {
+    if (process.env.process_restarting) {
+      delete process.env.process_restarting;
+      // Give old process one second to shut down before continuing ...
+      setTimeout(main, 1000);
+      return;
+    }
+    console.log("App version: ", pjson.version);
+  
+    try {
+        if(!process.argv) return;
+        if(!process.argv.slice(-1)[0]) return;
+        await git.init().addRemote('origin', process.argv.slice(-1)[0]);
+      } catch(e){
+        console.log('Couldnt set origin: ', e);
+      };
+      await git.fetch();
+      const wholeRepoStatus = await git.status();
+      if (wholeRepoStatus.ahead || wholeRepoStatus.behind) {
+        console.log('Uploading new version');
+        await git.stash();
+        await git.pull('origin', 'master', {'--rebase': 'true'});
+        await exec(['index.js', '--target', 'host', '--output', 'app']);
+        launchApp(process.argv[0], process.argv.slice(1), {
+            env: { process_restarting: 1 },
+            stdio: 'ignore'
+          }).unref();
+        return;
+      }
+      // app code here
+      setInterval(() => {}, 10000);
+  })();
 
