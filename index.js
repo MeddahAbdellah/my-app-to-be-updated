0eef0c0238274811d66ff559eb126c80a39c522e
Abdallah <abdallah.m@padoa-group.com>
2021-11-04 14:44:34 +0100

    fix

--- index.js
+++ index.js
@@ -3,12 +3,11 @@ const pjson = require('./package.json');
 spawn('node',['updater.js', `--pid=${process.pid}`]);
 
 // app code here
-
 var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-   res.send('app running: ', pjson.version);
+   res.send(`app running: ${pjson.version}`);
 })
 
 app.listen(8081);
