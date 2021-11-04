0366d2ac67c0695e4e09dd883168b148947fc313
Abdallah <abdallah.m@padoa-group.com>
2021-11-04 14:09:49 +0100

    fix

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const exec = require('child_process').exec;
 const pjson = require('./package.json');
-exec('node updater.js');
+exec(`node updater.js --pid=${process.pid}`);
 // app code here
 setInterval(() => { console.log('app running: ', pjson.version); }, 1000);
--- updater.js
+++ updater.js
@@ -2,11 +2,22 @@ const simpleGit = require('simple-git');
 const git = simpleGit();
 const { exec } = require('pkg');
 const execCommand = require('child_process').exec;
+const argv = key => {
+  // Return true if the key exists and a value is defined
+  if ( process.argv.includes( `--${ key }` ) ) return true;
+
+  const value = process.argv.find( element => element.startsWith( `--${ key }=` ) );
+
+  // Return null if the key does not exist and a value is not defined
+  if ( !value ) return null;
+  
+  return value.replace( `--${ key }=` , '' );
+}
 
 (async function main() {
   try {
       if(!process.argv) return;
-      if(!process.argv.slice(-1)[0]) return;
+      if(!argv('origin')) return;
       await git.init().addRemote('origin', process.argv.slice(-1)[0]);
     } catch(e){
       console.log('Couldnt set origin: ', e);
@@ -14,12 +25,12 @@ const execCommand = require('child_process').exec;
   await git.fetch();
   const wholeRepoStatus = await git.status();
   if (wholeRepoStatus.ahead || wholeRepoStatus.behind) {
-    execCommand('pm2 kill');
+    execCommand(`kill -9 ${argv('pid')}`);
     console.log('Uploading new version');
     await git.stash();
     await git.pull('origin', 'master', {'--rebase': 'true'});
     await exec(['index.js', '--target', 'host', '--output', 'app']);
-    execCommand('pm2 start ./app');
+    execCommand('./app');
     return;
   }
 })();
